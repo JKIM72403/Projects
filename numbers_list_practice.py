d44aabfeb5f947b71f22ffa8b97f40e485163db8
bhaylock-sphlv <71159670+bhaylock-sphlv@users.noreply.github.com>
2020-09-17 10:53:51 +0700

    NumbersAndLists-Feedback

---
+++ numbers_list_practice.py
@@ -0,0 +1,32 @@
+EngHL, GeoHL, Math, Econ, BI, Compsci = 5, 6, 6, 5, 6, 5
+Total = EngHL + GeoHL + Math + Econ + BI + Compsci
+Message1 = f"I predict my IB score in the DP program will be {Total}\n"
+print (Message1)
+
+Friends = ['jason', 'bara', "nathan"]
+# I wasnt sure whether to put it under a single print command becuase the instructions said one by one.
+print (Friends[0].title())
+print (Friends[1].title())
+print (Friends[2].title())
+
+# I think theres a way to do it in like half the lines but I dont know how to do that so I did the long way
+Message2 = f"Good Morning {Friends[0].title()}."
+Message3 = f"Good Morning {Friends[1].title()}."
+Message4 = f"Good Morning {Friends[2].title()}."
+
+print (Message2)
+print (Message3)
+print (Message4)
+
+#Same as last time, but i dont know how to make it cleaner with less lines. So i did it the long way
+Favresto = ['Mcdonalds', 'burgerking', 'Marzanos','flipburger']
+Message5 = f"I like {Favresto[0]} because of their Big Mac burger."
+Message6 = f"I like {Favresto[1]} because of their Whopper burger."
+Message7 = f"I like {Favresto[2]} because of their thin crust pizza."
+Message8 = f"I like {Favresto[3]} because of their cheese and bacon tatortots."
+
+print (Message5)
+print (Message6)
+print (Message7)
+print (Message8)
+
